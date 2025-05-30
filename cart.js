8a5427d5fd136a11de6e29a2618b9be35b623d8b
matteocesconITS <matteo.cescon@stud.itsaltoadriatico.it>
2025-05-30 10:30:16 +0200

    create js files

---
+++ cart.js
@@ -0,0 +1,52 @@
+const cartDiv = document.getElementById("cart");
+
+function renderCart() {
+  cartDiv.innerHTML = "";
+  const cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+  if (cart.length > 0) {
+    cart.forEach((item, index) => {
+      const itemDiv = document.createElement("div");
+      itemDiv.className = "cart-item";
+      itemDiv.innerHTML = `
+          <h2>${item.name}</h2>
+          <p><strong>Texture:</strong> ${item.texture}</p>
+          <p><strong>Price:</strong> $${item.price}</p>
+          <button onclick="removeItem(${index})" style="
+            background-color: #e74c3c;
+            color: white;
+            border: none;
+            padding: 8px 12px;
+            margin-top: 10px;
+            border-radius: 5px;
+            cursor: pointer;
+          ">Remove</button>
+        `;
+      cartDiv.appendChild(itemDiv);
+    });
+
+    // Buttons
+    const btns = document.createElement("div");
+    btns.className = "buttons";
+    btns.innerHTML = `
+        <button class="clear" onclick="clearCart()">Clear Cart</button>
+      `;
+    cartDiv.appendChild(btns);
+  } else {
+    cartDiv.innerHTML = "<p>Your cart is empty.</p>";
+  }
+}
+
+function removeItem(index) {
+  let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+  cart.splice(index, 1);
+  localStorage.setItem("cartItems", JSON.stringify(cart));
+  renderCart(); // Refresh UI
+}
+
+function clearCart() {
+  localStorage.removeItem("cartItems");
+  renderCart();
+}
+
+renderCart();
---
+++ index.js
@@ -0,0 +1,110 @@
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true);
+let scene,
+  shoeMesh,
+  selectedTexture = "set1";
+
+const createScene = async () => {
+  scene = new BABYLON.Scene(engine);
+
+  // Environment
+  scene.environmentTexture = new BABYLON.HDRCubeTexture(
+    "background.hdr",
+    scene,
+    512,
+    false,
+    true,
+    false,
+    true
+  );
+
+  scene.createDefaultSkybox(scene.environmentTexture, true, 1000);
+
+  // Camera
+  const camera = new BABYLON.ArcRotateCamera(
+    "Camera",
+    Math.PI / -3.5,
+    Math.PI / 2.5,
+    20,
+    new BABYLON.Vector3(0, 0.5, 0),
+    scene
+  );
+  camera.attachControl(canvas, true);
+
+  // Light
+  const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  );
+  light.intensity = 1.0;
+
+  // Load Model
+  const result = await BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    "./model/",
+    "scarpa.glb",
+    scene
+  );
+  shoeMesh = result.meshes.find((m) => m.material);
+  shoeMesh.scaling = new BABYLON.Vector3(6, 6, 6);
+
+  // Load default texture
+  setTexture("set1");
+
+  return scene;
+};
+
+// Set texture
+function setTexture(setName) {
+  selectedTexture = setName;
+
+  const path = `./textures/${setName}/shoe/`;
+
+  const mat = new BABYLON.StandardMaterial("shoeMat", scene);
+  mat.diffuseTexture = new BABYLON.Texture(path + "basecolor.png", scene);
+  mat.bumpTexture = new BABYLON.Texture(path + "normal.png", scene);
+
+  mat.diffuseTexture.vScale = -1;
+  mat.bumpTexture.vScale = -1;
+
+  shoeMesh.material = mat;
+}
+
+// Add to cart
+function addToCart() {
+  const item = {
+    name: "Nike Shoes",
+    texture: selectedTexture,
+    price: 129.99,
+  };
+
+  let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+  cart.push(item);
+
+  localStorage.setItem("cartItems", JSON.stringify(cart));
+
+  document.getElementById(
+    "modal-message"
+  ).innerText = `Added to cart: ${item.name} - ${item.texture}`;
+  document.getElementById("modal").style.display = "block";
+}
+
+// Close modal click on icon
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+// Close modal click outise
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
+createScene().then(() => {
+  engine.runRenderLoop(() => scene.render());
+});
+
+window.addEventListener("resize", () => engine.resize());
